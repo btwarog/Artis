667c1681cefa78e209029d08699cea444ad51eea
Bartłomiej Twaróg <b.twarog90@gmail.com>
2021-03-21 11:12:00 +0100

    corrected CachedBookmarkedArtistsDao

--- brainz/src/main/java/pl/btwarog/brainz/data/cache/dao/CachedBookmarkedArtistsDao.kt
+++ brainz/src/main/java/pl/btwarog/brainz/data/cache/dao/CachedBookmarkedArtistsDao.kt
@@ -14,15 +14,15 @@ abstract class CachedBookmarkedArtistsDao {
 	@Insert(onConflict = OnConflictStrategy.REPLACE)
 	abstract suspend fun addBookmarkedArtist(cachedBookmarkedArtistItem: CachedBookmarkedArtistItem): Long
 
-	@Query("SELECT id FROM $BOOKMARKED_ARTISTS_TABLE_NAME WHERE id = :artistId")
-	abstract suspend fun getBookmarkedArtist(artistId: String): Flow<List<String>>
+	@Query("SELECT id FROM $BOOKMARKED_ARTISTS_TABLE_NAME WHERE id IN (:ids)")
+	abstract suspend fun getAllBookmarkedArtistsWithIds(ids: List<String>): List<String>
+
+	@Query("DELETE FROM $BOOKMARKED_ARTISTS_TABLE_NAME WHERE id = :artistId")
+	abstract suspend fun deleteBookmarkedArtist(artistId: String): Int
+
+	@Query("SELECT * FROM $BOOKMARKED_ARTISTS_TABLE_NAME WHERE id = :artistId")
+	abstract suspend fun getBookmarkedArtist(artistId: String): CachedBookmarkedArtistItem?
 
 	@Query("SELECT * FROM $BOOKMARKED_ARTISTS_TABLE_NAME")
 	abstract fun getAllBookmarkedArtists(): Flow<List<CachedBookmarkedArtistItem>>
-
-	@Query("SELECT id FROM $BOOKMARKED_ARTISTS_TABLE_NAME WHERE id IN (:ids)")
-	abstract fun getAllBookmarkedArtistsWithIds(ids: List<String>): Flow<List<String>>
-
-	@Query("DELETE FROM $BOOKMARKED_ARTISTS_TABLE_NAME WHERE id = :artistId")
-	abstract suspend fun deleteBookmarkedArtist(artistId: String): Long
 }
