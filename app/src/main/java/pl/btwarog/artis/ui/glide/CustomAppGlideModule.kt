7031d00bdd69be2f8518f1b145c3725ba3ed4b6d
Bartłomiej Twaróg <b.twarog90@gmail.com>
2021-03-21 19:21:52 +0100

    unify artist font to placeholder one - sans-serif, add glide module

---
+++ app/src/main/java/pl/btwarog/artis/ui/glide/CustomAppGlideModule.kt
@@ -0,0 +1,7 @@
+package pl.btwarog.artis.ui.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class CustomAppGlideModule : AppGlideModule()
--- app/src/main/java/pl/btwarog/artis/ui/utils/Utils.kt
+++ app/src/main/java/pl/btwarog/artis/ui/utils/Utils.kt
@@ -1,11 +1,18 @@
 package pl.btwarog.artis.ui.utils
 
 import android.widget.ImageView
-import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import pl.btwarog.artis.R
+import pl.btwarog.artis.ui.glide.GlideApp
 
 fun ImageView.loadUrlImage(url: String) {
-	Glide.with(this).load(url).error(R.drawable.bg_no_image).centerCrop().into(this)
+	GlideApp.with(this)
+		.load(url)
+		.diskCacheStrategy(DiskCacheStrategy.ALL)
+		.centerCrop()
+		.dontAnimate()
+		.error(R.drawable.bg_no_image)
+		.into(this)
 }
 
 fun getBookmarkIconDrawable(bookmarked: Boolean) =
