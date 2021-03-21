2202f860701c978952367bec7a17a103fdf2011e
Bartłomiej Twaróg <b.twarog90@gmail.com>
2021-03-21 15:39:40 +0100

    added no image placeholder, changed list item to be more suitable

--- app/src/main/java/pl/btwarog/artis/ui/utils/Extensions.kt
+++ app/src/main/java/pl/btwarog/artis/ui/utils/Extensions.kt
@@ -2,7 +2,8 @@ package pl.btwarog.artis.ui.utils
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import pl.btwarog.artis.R
 
 fun ImageView.loadUrlImage(url: String) {
-	Glide.with(this).load(url).centerCrop().into(this)
+	Glide.with(this).load(url).error(R.drawable.bg_no_image).centerCrop().into(this)
 }
